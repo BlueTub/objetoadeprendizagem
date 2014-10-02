2effc3838921ca55006179eec9ca4edf463019de
pedropz0750@gmail.com <pedropz0750@gmail.com@30063ed2-7720-ea46-7b07-c17a3b0ffb28>
2014-10-02 15:02:41 +0000

    Programador mestre

---
+++ objetodeaprendizagem/src/controller/Metodos.java
@@ -0,0 +1,56 @@
+package controller;
+
+import java.awt.Rectangle;
+
+import javax.swing.JLabel;
+
+public class Metodos extends Thread{
+
+	private JLabel lbltext_1;
+	//private JLabel lbltext_2;
+	
+	public Metodos(JLabel lbltext_1, JLabel lbltext_2){
+		this.lbltext_1 = lbltext_1;
+		//this.lbltext_2 = lbltext_2;
+	}
+	
+	public void run (){
+		mexer();
+	}
+	
+	public void mexer(){
+		Rectangle text_1 = lbltext_1.getBounds();
+		//Rectangle text_2 = lbltext_2.getBounds();
+		
+		for (int i = 0; i < 4; i++) {
+			text_1.y += 20;
+			lbltext_1.setBounds(text_1);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		for (int i = 0; i < 6; i++) {
+			text_1.x += 20;
+			lbltext_1.setBounds(text_1);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		for (int i = 0; i < 2; i++) {
+			text_1.y -= 20;
+			lbltext_1.setBounds(text_1);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
