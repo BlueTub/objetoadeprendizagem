112a10c771c41f01be620465c2d1fc2b5574effb
hurygabriel@gmail.com <hurygabriel@gmail.com@30063ed2-7720-ea46-7b07-c17a3b0ffb28>
2014-10-08 23:06:51 +0000

    Criando arquivo de descrições, modificações no frm_principal.

--- objetodeaprendizagem/src/view/frm_principal.java
+++ objetodeaprendizagem/src/view/frm_principal.java
@@ -3,6 +3,14 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import java.awt.Panel;
+import javax.swing.JLabel;
 
 public class frm_principal {
 
@@ -36,8 +44,43 @@ public class frm_principal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 827, 478);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btn_inicia = new JButton("New button");
+		btn_inicia.setBounds(356, 23, 89, 40);
+		frame.getContentPane().add(btn_inicia);
+		
+		Panel panel = new Panel();
+		panel.setBounds(30, 111, 180, 149);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JButton btn_fila = new JButton("Fila");
+		btn_fila.setBounds(29, 11, 120, 35);
+		panel.add(btn_fila);
+		
+		JButton btn_lista = new JButton("Lista");
+		btn_lista.setBounds(29, 57, 120, 35);
+		panel.add(btn_lista);
+		
+		JButton btn_pilha = new JButton("Pilha");
+		btn_pilha.setBounds(29, 103, 120, 35);
+		panel.add(btn_pilha);
+		
+		JLabel lblNewLabel = new JLabel("lbl_titulo");
+		lblNewLabel.setBounds(294, 111, 277, 14);
+		frame.getContentPane().add(lblNewLabel);
+		btn_fila.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
+		menuBar.add(mntmNewMenuItem);
 	}
-
 }
