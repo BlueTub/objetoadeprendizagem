cac44fe38080fcf784d34c430a16a89f30ae17ce
hurygabriel@gmail.com <hurygabriel@gmail.com@30063ed2-7720-ea46-7b07-c17a3b0ffb28>
2014-10-08 23:11:46 +0000

    Criação do pacote imagens, criação do frm_sobre

---
+++ objetodeaprendizagem/src/view/fmz_sobre.java
@@ -0,0 +1,43 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class fmz_sobre {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					fmz_sobre window = new fmz_sobre();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public fmz_sobre() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
