bbb0ed6b6d92c58bdeecb60f8687fd3928c65e97
joao.victor.alves.jesus@gmail.com <joao.victor.alves.jesus@gmail.com@30063ed2-7720-ea46-7b07-c17a3b0ffb28>
2014-10-08 01:18:46 +0000

    07/10 algumas alterações

---
+++ objetodeaprendizagem/src/controller/No.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package controller;
+
+/**
+ * @author joão
+ *
+ */
+public class No {
+	public Perguntas dados;
+	public No prox;
+	
+	public No(Perguntas e){
+		dados=e;
+		prox=null;
+	}
+}
