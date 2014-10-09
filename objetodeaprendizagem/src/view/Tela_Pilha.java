2a3ef4ea90306b2fcc8eb61a7baf196de9306f5d
pedropz0750@gmail.com <pedropz0750@gmail.com@30063ed2-7720-ea46-7b07-c17a3b0ffb28>
2014-10-09 03:36:47 +0000

    Animação Pilha (Incompleta)

--- objetodeaprendizagem/src/view/Tela_Pilha.java
+++ objetodeaprendizagem/src/view/Tela_Pilha.java
@@ -2,6 +2,7 @@ package view;
 
 
 import java.awt.EventQueue;
+import java.awt.Window;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -9,7 +10,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 
-import controller.Metodos;
+import controller.AnimaçaoPilha;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,18 +24,28 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import java.awt.Window.Type;
 
 public class Tela_Pilha extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	private JTextField tf0;
+	private JTextField tf1;
+	private JTextField tf2;
+	private JTextField tf3;
+	private JTextField tf4;
+	private JTextField tf5;
 	private JMenuBar menuBar;
 	private final Action actionPrincipal = new MenuPrincipal();
+	private final Action actionSair = new sair();
+	private JTextField tf6;
+	private JLabel lbl_valor10;
+	private JLabel lbl_valor20;
+	private JLabel lbl_valor30;
+	private JLabel lbl_valor40;
+	private JLabel lbl_valor50;
+	private JLabel lbl_valor60;
+	private JLabel lbl_valor70;
 
 	/**
 	 * Launch the application.
@@ -57,6 +68,8 @@ public class Tela_Pilha extends JFrame {
 	 * Create the frame.
 	 */
 	public Tela_Pilha() {
+		setType(Type.UTILITY);
+		setResizable(false);
 		setTitle("Anima\u00E7\u00E3o - Pilha");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 398);
@@ -72,76 +85,137 @@ public class Tela_Pilha extends JFrame {
 		mnArquivo.add(mntmMenuPrincipal);
 		
 		JMenuItem mntmSair = new JMenuItem("Sair");
+		mntmSair.setAction(actionSair);
 		mnArquivo.add(mntmSair);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		lbl_valor10 = new JLabel("10");
+		lbl_valor10.setVisible(false);
+		lbl_valor10.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lbl_valor10.setForeground(Color.BLACK);
+		lbl_valor10.setBounds(163, 54, 29, 38);
+		contentPane.add(lbl_valor10);
+		
+		lbl_valor70 = new JLabel("70");
+		lbl_valor70.setForeground(Color.BLACK);
+		lbl_valor70.setVisible(false);
+		lbl_valor70.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lbl_valor70.setBounds(163, 58, 29, 31);
+		contentPane.add(lbl_valor70);
+		
+		lbl_valor60 = new JLabel("60");
+		lbl_valor60.setForeground(Color.BLACK);
+		lbl_valor60.setVisible(false);
+		lbl_valor60.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lbl_valor60.setBounds(163, 58, 29, 31);
+		contentPane.add(lbl_valor60);
+		
+		lbl_valor50 = new JLabel("50");
+		lbl_valor50.setForeground(Color.BLACK);
+		lbl_valor50.setVisible(false);
+		lbl_valor50.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lbl_valor50.setBounds(163, 58, 29, 31);
+		contentPane.add(lbl_valor50);
+		
+		lbl_valor40 = new JLabel("40");
+		lbl_valor40.setForeground(Color.BLACK);
+		lbl_valor40.setVisible(false);
+		lbl_valor40.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lbl_valor40.setBounds(163, 58, 29, 31);
+		contentPane.add(lbl_valor40);
+		
+		lbl_valor30 = new JLabel("30");
+		lbl_valor30.setForeground(Color.BLACK);
+		lbl_valor30.setVisible(false);
+		lbl_valor30.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lbl_valor30.setBounds(163, 58, 29, 31);
+		contentPane.add(lbl_valor30);
+		
+		lbl_valor20 = new JLabel("20");
+		lbl_valor20.setForeground(Color.BLACK);
+		lbl_valor20.setVisible(false);
+		lbl_valor20.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lbl_valor20.setBounds(163, 58, 29, 31);
+		contentPane.add(lbl_valor20);
+		
+		
 		
 		
 		final JButton btnAçao = new JButton("Adicionar");
-		btnAçao.setBounds(178, 299, 89, 23);
+		btnAçao.setBounds(275, 299, 89, 23);
 		contentPane.add(btnAçao);
 
-		textField = new JTextField();
-		textField.setBackground(Color.LIGHT_GRAY);
-		textField.setEditable(false);
-		textField.setBounds(60, 291, 63, 38);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		tf0 = new JTextField();
+		tf0.setBackground(Color.LIGHT_GRAY);
+		tf0.setEditable(false);
+		tf0.setBounds(60, 291, 63, 38);
+		contentPane.add(tf0);
+		tf0.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setEditable(false);
-		textField_1.setColumns(10);
-		textField_1.setBackground(Color.LIGHT_GRAY);
-		textField_1.setBounds(60, 252, 63, 38);
-		contentPane.add(textField_1);
+		tf1 = new JTextField();
+		tf1.setEditable(false);
+		tf1.setColumns(10);
+		tf1.setBackground(Color.LIGHT_GRAY);
+		tf1.setBounds(60, 252, 63, 38);
+		contentPane.add(tf1);
 		
-		textField_2 = new JTextField();
-		textField_2.setEditable(false);
-		textField_2.setColumns(10);
-		textField_2.setBackground(Color.LIGHT_GRAY);
-		textField_2.setBounds(60, 213, 63, 38);
-		contentPane.add(textField_2);
+		tf2 = new JTextField();
+		tf2.setEditable(false);
+		tf2.setColumns(10);
+		tf2.setBackground(Color.LIGHT_GRAY);
+		tf2.setBounds(60, 213, 63, 38);
+		contentPane.add(tf2);
 		
-		textField_3 = new JTextField();
-		textField_3.setEditable(false);
-		textField_3.setColumns(10);
-		textField_3.setBackground(Color.LIGHT_GRAY);
-		textField_3.setBounds(60, 174, 63, 38);
-		contentPane.add(textField_3);
+		tf3 = new JTextField();
+		tf3.setEditable(false);
+		tf3.setColumns(10);
+		tf3.setBackground(Color.LIGHT_GRAY);
+		tf3.setBounds(60, 174, 63, 38);
+		contentPane.add(tf3);
 		
-		textField_4 = new JTextField();
-		textField_4.setEditable(false);
-		textField_4.setColumns(10);
-		textField_4.setBackground(Color.LIGHT_GRAY);
-		textField_4.setBounds(60, 135, 63, 38);
-		contentPane.add(textField_4);
+		tf4 = new JTextField();
+		tf4.setEditable(false);
+		tf4.setColumns(10);
+		tf4.setBackground(Color.LIGHT_GRAY);
+		tf4.setBounds(60, 135, 63, 38);
+		contentPane.add(tf4);
 		
-		textField_5 = new JTextField();
-		textField_5.setEditable(false);
-		textField_5.setColumns(10);
-		textField_5.setBackground(Color.LIGHT_GRAY);
-		textField_5.setBounds(60, 96, 63, 38);
-		contentPane.add(textField_5);
+		tf5 = new JTextField();
+		tf5.setEditable(false);
+		tf5.setColumns(10);
+		tf5.setBackground(Color.LIGHT_GRAY);
+		tf5.setBounds(60, 96, 63, 38);
+		contentPane.add(tf5);
 		
-		JLabel lblFuncionamentoDeUma = new JLabel("PILHA");
-		lblFuncionamentoDeUma.setFont(new Font("Tahoma", Font.BOLD, 26));
-		lblFuncionamentoDeUma.setBounds(50, 47, 96, 28);
-		contentPane.add(lblFuncionamentoDeUma);
+		JLabel lblTitulo = new JLabel("PILHA");
+		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 26));
+		lblTitulo.setBounds(46, 11, 96, 28);
+		contentPane.add(lblTitulo);
+		
+		tf6 = new JTextField();
+		tf6.setEditable(false);
+		tf6.setColumns(10);
+		tf6.setBackground(Color.LIGHT_GRAY);
+		tf6.setBounds(60, 57, 63, 38);
+		contentPane.add(tf6);
 		
 		
-		ActionListener iniciar = new ActionListener() {
+		ActionListener açao = new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				Thread t1 = new AnimaçaoPilha(lbl_valor10, lbl_valor20, lbl_valor30, 
+						lbl_valor40, lbl_valor50, lbl_valor60, lbl_valor70);
+				t1.start();
 				//btnIniciar.setText("Remover");
 				
 			}
 		};
 		
-		btnAçao.addActionListener(iniciar);
+		btnAçao.addActionListener(açao);
 		
 	}
 	private class MenuPrincipal extends AbstractAction {
@@ -151,9 +225,18 @@ public class Tela_Pilha extends JFrame {
 		}
 		public void actionPerformed(ActionEvent e) {
 			Tela_Pilha.this.dispose();
-			frm_principal principal = new frm_principal();
-			principal.setVisible(true); //AQUI QUE ESSA PORRA DA ERRO !!!!
+			frm_principal prin = new frm_principal();
+			//prin.setVisible(true);
 		}
 		
+	}
+	private class sair extends AbstractAction {
+		public sair() {
+			putValue(NAME, "Sair");
+			putValue(SHORT_DESCRIPTION, "Fechar aplicação");
+		}
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
 	}
 }
