b65bb677cf5f0f07b7ec0922322bf32b4ad94453
pedroafonsosilva07 <pedroafonsosilva07@gmail.com>
2014-10-10 12:27:11 +0000

    Criacao do metodo Pontuacao na classe de carregamento

--- objetodeaprendizagem/src/controller/ThreadCarregamento.java
+++ objetodeaprendizagem/src/controller/ThreadCarregamento.java
@@ -4,6 +4,7 @@
 package controller;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JProgressBar;
 
 /**
@@ -14,26 +15,31 @@ import javax.swing.JProgressBar;
 public class ThreadCarregamento extends Thread{
 	  private JProgressBar barra=new JProgressBar();
 	  private JLabel texto=new JLabel();
+	  private JLabel dica = new JLabel();
+	  private JLabel pontuacao = new JLabel();
+
 	    
 	  /**
 	   * Construtor 
 	   * @param barra 
 	   * @param texto
 	   */
-	    public ThreadCarregamento(JProgressBar barra,JLabel texto){
+	    public ThreadCarregamento(JProgressBar barra,JLabel texto,JLabel dica,JLabel pontuacao){
 	        this.barra=barra;
 	        this.texto=texto;
+	        this.dica=dica;
+	        this.pontuacao=pontuacao;
 	    }
 	    
 	    @Override
 	    public void run(){
-	        Carrega(); 
+	    	Carrega(Potuacao()); 
 	        
 	    }
 	    /**
 	     * Método para carregar a barra
 	     */
-	    public void Carrega(){
+	    public void Carrega(int e){
 	        for(int i=0;i<=100;i++){
 	            barra.setValue(i);
 	            try {
@@ -51,7 +57,34 @@ public class ThreadCarregamento extends Thread{
 	                }
 	            }  
 	        }
+	        
+	        
+	        String resp = Integer.toString(e);
+	        pontuacao.setText(resp);
+	        pontuacao.setVisible(true);
+	        
 	    
+	    }
+	    /**
+	     * Metodo para gerar a pontuacao das perguntas e pegar qual perguntas ele errou.
+	     * @return pontuacao.
+	     */
+	    public int Potuacao(){
+	    	Fila f = new Fila();
+	    	Pilha p = new Pilha();
+	    	int pont = 0;
+	    	int resp = 0;
+	    	
+	    	for (int i = 0; i < 5; i++) {
+				if(p.desempilha() == f.remove()){
+					pont = pont + 20;
+					resp = i;//para dar a dica e saber ql a resposta foi errada
+				} else {
+					resp = i;
+				}
+			}
+	    	
+	    	return pont;
 	    }
 	    
 
