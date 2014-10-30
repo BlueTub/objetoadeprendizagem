f0c19c02f4cd236488ebbe25ed3b7b40d2b7701d
hurygabriel <hurygabriel@gmail.com>
2014-10-30 18:01:25 +0000

    Criação Boundary Frm_animaArvore.

---
+++ objetodeaprendizagem/src/view/Frm_animaArvore.java
@@ -0,0 +1,45 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import controller.Telas;
+
+@SuppressWarnings("serial")
+public class Frm_animaArvore extends Telas {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frm_animaArvore frame = new Frm_animaArvore();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Frm_animaArvore() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
