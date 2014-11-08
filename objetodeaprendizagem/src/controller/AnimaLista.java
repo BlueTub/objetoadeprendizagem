a60541623ca7a30fb3622e57a57eedc6d5a465f5
hurygabriel <hurygabriel@gmail.com>
2014-11-08 02:57:36 +0000

    Adicionados Elementos Visuais, Lista em trabalho.

--- objetodeaprendizagem/src/controller/AnimaLista.java
+++ objetodeaprendizagem/src/controller/AnimaLista.java
@@ -1,5 +1,17 @@
 package controller;
 
-public class AnimaLista {
+public class AnimaLista extends Thread implements OperacaoAnimacao {
+
+	@Override
+	public void addElemento() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeElemento() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
