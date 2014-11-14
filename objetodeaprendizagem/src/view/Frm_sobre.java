e8527e958d1827b8fe08fe391b91a935b208061e
joao.victor.alves.jesus <joao.victor.alves.jesus@gmail.com>
2014-11-14 23:17:15 +0000

    arrumando as telas para o visual motif

--- objetodeaprendizagem/src/view/Frm_sobre.java
+++ objetodeaprendizagem/src/view/Frm_sobre.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -50,7 +51,7 @@ public class Frm_sobre extends Telas {
 		setTitle("Sobre");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 444);
+		setBounds(100, 100, 450, 464);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -62,21 +63,23 @@ public class Frm_sobre extends Telas {
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(Frm_sobre.class.getResource("/Imagens/fatec_logo.png")));
-		lblNewLabel.setBounds(128, 11, 158, 67);
+		lblNewLabel.setBounds(128, 0, 158, 67);
 		contentPane.add(lblNewLabel);
 		
 		JTextPane txtpnProjetoDesenvolvidoNa = new JTextPane();
 		txtpnProjetoDesenvolvidoNa.setBackground(UIManager.getColor("InternalFrame.activeTitleGradient"));
 		txtpnProjetoDesenvolvidoNa.setEditable(false);
-		txtpnProjetoDesenvolvidoNa.setText("Projeto desenvolvido na institui\u00E7\u00E3o FATEC da zona leste, no 3\u00BA semestre do curso de analise e desenvolvimento de sistemas sob orienta\u00E7\u00E3o dos prof(a) ms. Andreia, Cristina e Wellingtton");
-		txtpnProjetoDesenvolvidoNa.setBounds(61, 93, 316, 78);
+		txtpnProjetoDesenvolvidoNa.setText("Projeto desenvolvido na institui\u00E7\u00E3o FATEC da zona leste, no 3\u00BA semestre do curso de análise e desenvolvimento de sistemas sob orienta\u00E7\u00E3o dos prof(a) ms. Andreia, Cristina e Wellingtton");
+		txtpnProjetoDesenvolvidoNa.setBounds(61, 61, 316, 106);
+		txtpnProjetoDesenvolvidoNa.setFont(new Font("Tahoma",Font.PLAIN,tamanhofonte));
 		contentPane.add(txtpnProjetoDesenvolvidoNa);
 		
 		JTextPane txtpnDesenvolvedoresDoProjeto = new JTextPane();
-		txtpnDesenvolvedoresDoProjeto.setBackground(UIManager.getColor("InternalFrame.activeTitleGradient"));
 		txtpnDesenvolvedoresDoProjeto.setEditable(false);
-		txtpnDesenvolvedoresDoProjeto.setText("                     Desenvolvedores do projeto: \r\n\r\n                      Hury Gabriel A.B de Jesus\r\n                      Pedro Zuzi \r\n                      Pedro Afonso\r\n                      Jo\u00E3o Victor\r\n                      Leonardo Benedicto\r\n\r\n                      S\u00E3o Paulo/2014");
-		txtpnDesenvolvedoresDoProjeto.setBounds(61, 182, 316, 158);
+		txtpnDesenvolvedoresDoProjeto.setFont(new Font("Tahoma", Font.PLAIN, tamanhofonte));
+		txtpnDesenvolvedoresDoProjeto.setBackground(UIManager.getColor("InternalFrame.activeTitleGradient"));
+		txtpnDesenvolvedoresDoProjeto.setText("                 Desenvolvedores: \r\n                 Hury Gabriel A.B de Jesus\r\n                 Pedro Zuzi \r\n                 Pedro Afonso\r\n                 Jo\u00E3o Victor Alves de Jesus\r\n                 Leonardo Benedicto\r\n\r\n                 S\u00E3o Paulo/2014");
+		txtpnDesenvolvedoresDoProjeto.setBounds(61, 182, 316, 166);
 		contentPane.add(txtpnDesenvolvedoresDoProjeto);
 		
 		JButton btnNewButton = new JButton("");
@@ -87,16 +90,17 @@ public class Frm_sobre extends Telas {
 				dispose();
 			}
 		});
-		btnNewButton.setBounds(178, 351, 89, 39);
+		btnNewButton.setBounds(178, 351, 89, 57);
 		contentPane.add(btnNewButton);
 		
 		JLabel lblVoltar = new JLabel("Voltar");
-		lblVoltar.setBounds(202, 390, 46, 14);
+		lblVoltar.setFont(new Font("Tahoma",Font.PLAIN,tamanhofonte));
+		lblVoltar.setBounds(202, 415, 46, 14);
 		contentPane.add(lblVoltar);
 		
 		JLabel lbl_fundo = new JLabel("");
 		lbl_fundo.setIcon(new ImageIcon(Frm_sobre.class.getResource("/Imagens/BackGround.png")));
-		lbl_fundo.setBounds(0, 0, 444, 415);
+		lbl_fundo.setBounds(0, 0, 444, 435);
 		contentPane.add(lbl_fundo);
 	}
 }
