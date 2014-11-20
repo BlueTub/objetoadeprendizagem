ec087f2bd06cb1cf5651577d8efaabf70080d3c9
joao.victor.alves.jesus <joao.victor.alves.jesus@gmail.com>
2014-11-20 16:38:34 +0000

    sobre interativo para ED

--- objetodeaprendizagem/src/view/Frm_sobre.java
+++ objetodeaprendizagem/src/view/Frm_sobre.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.awt.Desktop;
 import java.awt.EventQueue;
 import java.awt.Font;
 
@@ -8,6 +9,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import javax.swing.JTextPane;
 import javax.swing.UIManager;
 import javax.swing.JButton;
@@ -16,6 +18,11 @@ import controller.Telas;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 /**
  * Mostra o sobre da aplicação
  * @author Hury
@@ -25,7 +32,6 @@ import java.awt.event.ActionEvent;
 public class Frm_sobre extends Telas {
 
 	private JPanel contentPane;
-
 	/**
 	 * Launch the application.
 	 */
@@ -61,7 +67,112 @@ public class Frm_sobre extends Telas {
 		Estilo();
 		iconeBarra();
 		
+		JLabel lblDesenvolvedores = new JLabel("Desenvolvedores:");
+		lblDesenvolvedores.setFont(new Font("Tahoma", Font.BOLD, tamanhofonte));
+		lblDesenvolvedores.setBounds(128, 184, 173, 14);
+		contentPane.add(lblDesenvolvedores);
+		
+		JLabel lblHury = new JLabel("<html><u>Hury Gabriel A.B de Jesus</u></html>");
+		lblHury.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0){
+				URI url = null;
+				try {
+					url = new URI("https://www.facebook.com/hurygabriel");
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				}
+				abrirUrl(url);
+			}
+		});
+		lblHury.setFont(new Font("Tahoma", Font.BOLD, tamanhofonte));
+		lblHury.setBounds(128, 219, 250, 21);
+		contentPane.add(lblHury);
+		
+		JLabel lblJoao = new JLabel("<html><u>Jo\u00E3o Victor Alves de Jesus</u></html>");
+		lblJoao.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				URI url = null;
+				try {
+					url = new URI("https://www.facebook.com/joao.victor.alves.jesus");
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				}
+				abrirUrl(url);
+			}
+		});
+		lblJoao.setFont(new Font("Tahoma", Font.BOLD, tamanhofonte));
+		lblJoao.setBounds(128, 240, 250, 21);
+		contentPane.add(lblJoao);
+		
+		JLabel lblZuzi = new JLabel("<html><u>Pedro Zuzi</u></html>");
+		lblZuzi.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+					URI url = null;
+					try {
+						url = new URI("https://www.facebook.com/pedro.zuzi");
+					} catch (URISyntaxException e) {
+						e.printStackTrace();
+					}
+					abrirUrl(url);
+			}
+		});
+		lblZuzi.setFont(new Font("Tahoma", Font.BOLD, tamanhofonte));
+		lblZuzi.setBounds(128, 280, 197, 21);
+		contentPane.add(lblZuzi);
+		
+		JLabel lblBurns = new JLabel("<html><u>Leonardo Benedicto</u></html>");
+		lblBurns.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				URI url = null;
+				try {
+					url = new URI("https://www.facebook.com/leonardo.leitebenedicto");
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				}
+				abrirUrl(url);
+			}
+		});
+		lblBurns.setFont(new Font("Tahoma", Font.BOLD, tamanhofonte));
+		lblBurns.setBounds(128, 197, 197, 21);
+		contentPane.add(lblBurns);
+		
+		JLabel lblCorinthians = new JLabel("<html><u> Pedro Afonso</u></html>");
+		lblCorinthians.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				URI url = null;
+				try {
+					url = new URI("https://www.facebook.com/pedroamellosilva");
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				}
+				abrirUrl(url);
+				
+			}
+		});
+		lblCorinthians.setFont(new Font("Tahoma", Font.BOLD, tamanhofonte));
+		lblCorinthians.setBounds(128, 261, 197, 21);
+		contentPane.add(lblCorinthians);
+
+		
 		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				URI url = null;
+				try {
+					url = new URI("http://www.fateczl.edu.br/");
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				}
+				abrirUrl(url);
+				
+			}
+		});
 		lblNewLabel.setIcon(new ImageIcon(Frm_sobre.class.getResource("/Imagens/fatec_logo.png")));
 		lblNewLabel.setBounds(128, 0, 158, 67);
 		contentPane.add(lblNewLabel);
@@ -70,17 +181,9 @@ public class Frm_sobre extends Telas {
 		txtpnProjetoDesenvolvidoNa.setBackground(UIManager.getColor("InternalFrame.activeTitleGradient"));
 		txtpnProjetoDesenvolvidoNa.setEditable(false);
 		txtpnProjetoDesenvolvidoNa.setText("Projeto desenvolvido na institui\u00E7\u00E3o FATEC da zona leste, no 3\u00BA semestre do curso de análise e desenvolvimento de sistemas sob orienta\u00E7\u00E3o dos prof(a) ms. Andreia, Cristina e Wellingtton");
-		txtpnProjetoDesenvolvidoNa.setBounds(61, 61, 316, 106);
+		txtpnProjetoDesenvolvidoNa.setBounds(62, 67, 316, 106);
 		txtpnProjetoDesenvolvidoNa.setFont(new Font("Tahoma",Font.PLAIN,tamanhofonte));
 		contentPane.add(txtpnProjetoDesenvolvidoNa);
-		
-		JTextPane txtpnDesenvolvedoresDoProjeto = new JTextPane();
-		txtpnDesenvolvedoresDoProjeto.setEditable(false);
-		txtpnDesenvolvedoresDoProjeto.setFont(new Font("Tahoma", Font.PLAIN, tamanhofonte));
-		txtpnDesenvolvedoresDoProjeto.setBackground(UIManager.getColor("InternalFrame.activeTitleGradient"));
-		txtpnDesenvolvedoresDoProjeto.setText("                 Desenvolvedores: \r\n                 Hury Gabriel A.B de Jesus\r\n                 Pedro Zuzi \r\n                 Pedro Afonso\r\n                 Jo\u00E3o Victor Alves de Jesus\r\n                 Leonardo Benedicto\r\n\r\n                 S\u00E3o Paulo/2014");
-		txtpnDesenvolvedoresDoProjeto.setBounds(61, 182, 316, 166);
-		contentPane.add(txtpnDesenvolvedoresDoProjeto);
 		
 		JButton btnNewButton = new JButton("");
 		btnNewButton.setSelectedIcon(new ImageIcon(Frm_sobre.class.getResource("/Imagens/arrow-back-select.png")));
@@ -90,7 +193,7 @@ public class Frm_sobre extends Telas {
 				dispose();
 			}
 		});
-		btnNewButton.setBounds(178, 351, 89, 57);
+		btnNewButton.setBounds(162, 337, 89, 57);
 		contentPane.add(btnNewButton);
 		
 		JLabel lblVoltar = new JLabel("Voltar");
@@ -103,4 +206,21 @@ public class Frm_sobre extends Telas {
 		lbl_fundo.setBounds(0, 0, 444, 435);
 		contentPane.add(lbl_fundo);
 	}
+	/**
+	 * Método para abrir uma url pelo aplicativo padrão do SO
+	 * @param caminho Caminho da url
+	 */
+	public void abrirUrl(URI caminho){
+		Desktop desktop = Desktop.getDesktop();
+		try {
+			boolean abrir = desktop.isSupported(Desktop.Action.BROWSE);
+			if (abrir) {
+				desktop.browse(caminho);
+			}
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null,
+					"<html>Seu computador não possui nenhum browser<br></html>");
+		}
+	}
+	
 }
